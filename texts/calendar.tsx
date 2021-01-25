eb76bc015952b320513c83de7b1adc512f51cbca
nibdo <hello@nibdo.com>
2021-01-25 20:41:56 +0100

    update texts

--- texts/calendar.tsx
+++ texts/calendar.tsx
@@ -7,7 +7,7 @@ const CalendarText = () => {
       Your data are encrypted in
       browser with your password, which is never sent to server. You may be familiar with
       this concept with great service from ProtonMail (btw, check ProtonMail and Tutanota encrypted calendars too). <br/><br/>
-      Only small part of data like event's date are in plain text so repeated events and notifications can be handled from the server.
+      Only small part of data like dates and timezones for events and calendars are in plain text. This way notifications and repeated events be handled from the server.
       One trade-off yet to solve is way around easier sharing of your calendars.
         Bloben Calendar is open sourced and you can view code on <a className={'web-link'} target={'_blank'} href={'https://github.com/nibdo/bloben-calendar'}>Github</a>.
     <br/><br/>
