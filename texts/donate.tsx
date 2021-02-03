2656dfc4058d7ea775af58ebcabe1e05f750bf46
nibdo <hello@nibdo.com>
2021-02-03 19:58:09 +0100

    typo

--- texts/donate.tsx
+++ texts/donate.tsx
@@ -4,7 +4,7 @@ const DonateText = () => {
   return (
     <p>Developing Bloben is my hobby and not source of income, so you are free to use it if you find application useful.
    <br/><br/>
-   Formally I offered Bloben as service on my server, but decided to continue only as self hosted project so this cut some costs. If anyone will still want to donate, I offer few options through Bitcoin, Patreon or Buy me coffee
+    Formerly I offered Bloben as service on my server, but decided to continue only as self hosted project so this cut some costs. If anyone will still want to donate, I offer few options through Bitcoin, Patreon or Buy me coffee
    Thank you!</p>
   )
  }
